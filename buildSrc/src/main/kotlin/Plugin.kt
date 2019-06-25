fa8b3f4dd4ac865aebb5b74fb49a6c445b70d70d
Björn Petersen <pheasn@gmail.com>
2019-06-25 12:53:10 +0200

    Update dependency versions

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,7 +1,7 @@
 object Plugin {
-    const val SPOTLESS = "3.23.0"
+    const val SPOTLESS = "3.23.1"
     const val VERSIONS = "0.21.0"
 
-    const val KOTLIN = "1.3.31"
+    const val KOTLIN = "1.3.40"
     const val SHADOW_JAR = "5.0.0"
 }
