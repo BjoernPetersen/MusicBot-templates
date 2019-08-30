f685b0de27295bd0c268c22ac35dfe2c2e35da0f
Björn Petersen <pheasn@gmail.com>
2019-08-30 13:46:47 +0200

    Use MusicBot 0.23.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,7 +1,7 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
 
-    const val MUSICBOT = "0.22.0"
+    const val MUSICBOT = "0.23.0"
 
     const val SLF4J = "1.8.0-beta4"
     const val JUNIT = "5.5.1"
